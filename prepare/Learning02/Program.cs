b270dfe717498061f721b8866b8f5dd51d33b5df
Orlando Ancelmo Pinelo <orlandoancelmopinelo@gmail.com>
2023-05-13 23:07:15 -0600

    ¨x¨

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,6 +5,8 @@ class Program
     static void Main(string[] args)
     {
        // Console.WriteLine("Hello Learning02 World!");
+              // Console.WriteLine("Hello Learning02 World!");
+
        Job job1 = new Job();
            job1._company="(Micrsoft)";
            job1._jobTitle="Software Engineer";

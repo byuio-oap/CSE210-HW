7724574654a63b69198c7677202b7abe0ba01761
Orlando Ancelmo Pinelo <orlandoancelmopinelo@gmail.com>
2023-05-17 21:31:55 -0600

    encapsulation learning upload

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        //Console.WriteLine("Hello Learning03 World!");
+         Fraction noParameter = new Fraction();
+        Console.WriteLine(noParameter.GetFractionString());
+        Console.WriteLine(noParameter.GetDecimalValue());
+
+        Fraction singleParameter = new Fraction(5);
+        Console.WriteLine(singleParameter.GetFractionString());
+        Console.WriteLine(singleParameter.GetDecimalValue());
+
+        Fraction doubleParmeter = new Fraction(3, 4);
+        Console.WriteLine(doubleParmeter.GetFractionString());
+        Console.WriteLine(doubleParmeter.GetDecimalValue());
+
+        Fraction doubleParmeter2 = new Fraction(1, 3);
+        Console.WriteLine(doubleParmeter2.GetFractionString());
+        Console.WriteLine(doubleParmeter2.GetDecimalValue());  
     }
 }

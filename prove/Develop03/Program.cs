ee879d1963a3aa461d272cddf88550c4d91d396a
Orlando Ancelmo Pinelo <orlandoancelmopinelo@gmail.com>
2023-05-27 22:46:06 -0600

    Learning Encapsualtio by dev Scriptue memorization

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,41 @@
 using System;
 
+
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+     Reference clsReference = new Reference();
+     clsReference.BuildScriptureRefrence();
+     string scriptReference=clsReference.GetReference();
+    // clsReference.DisplayScriptureReference();
+    Word word = new Word();
+     Scriptures clsScripture = new Scriptures();
+     clsScripture.BuildScripture();
+     string scriptre=clsScripture.GetScripture();
+     word.GetRenderedText(clsScripture);
+  //  word.GetRenderedRef(clsScripture);
+     //Console.WriteLine($"{refer}  {script}");
+     
+     while (word._hiddenWords.Count < word._arrayofWords.Length)
+                    {
+                        word.ShowScripture(scriptReference);
+                        word.GetReadKey();
+                    }
+                    word.ShowScripture(scriptReference);
+    
+
+
+     
+
     }
+
 }
+
+    
+
+   
+
+
